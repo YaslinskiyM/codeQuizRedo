168af273da35c549047de738a15b570d2458ede8
maria <mariayaslinskiy@gmail.com>
2023-07-12 14:19:35 -0700

    redo of this

---
+++ Assets/script.js
@@ -0,0 +1,210 @@
+// Created variables
+var timeElement = document.querySelector("#time");
+var wrapperElement = document.querySelector(".wrapper");
+var btnElement = document.querySelector("#start");
+var divContEL = document.querySelector(".divContainer");
+var hElement = document.querySelector("#title");
+var orderListEl = document.querySelector("#q-list");
+var finishDiv = document.querySelector(".finish-section");
+var finalScore = document.querySelector("#result");
+var errMsg = document.querySelector("#errorSmg");
+var initialInput = document.querySelector("#inputInitial").value;
+var submitEl = document.querySelector(".btn btn-primary mb-2");
+var responsDiv = document.querySelector("#response");
+var finaPageEl = document.querySelector(".final-page");
+var initialAndScore = document.querySelector("#staticEmail");
+var firstPageEl = document.querySelector(".first-page");
+
+// Created an array of questions
+var questions = [
+    {
+        title: "What is the capital city of France?:",
+        choices: ["Mexico City", "Paradise", "Lynwood", "Paris"],
+        answer: "Paris",
+    },
+    {
+        title: "Which planet is known as the Red Planet?:",
+        choices: ["Earth", "Mars", "Luna", "Jupiter"],
+        answer: "Mars",
+    },
+    {
+        title: "Who wrote the famous play Romeo and Juliet?:",
+        choices: ["William Shakespeare", "Donald Trump", "Jesus", "Ashton Kutcher"],
+        answer: "William Shakespeare",
+    },
+    {
+        title: " How many continents are there on Earth?",
+        choices: ["none", "12", "28", "7"],
+
+        answer: "7",
+    },
+    {   
+        title: "What is the result of multiplying 7 by 8",
+        choices: ["28", "56", "14", "42"],
+        answer: "56",
+    }
+]
+
+// Created questions to add to HTML dynamically
+ function displayQuestions() {
+    var holdQ1Title = questions[i].title
+    hElement.textContent = holdQ1Title
+    var holdq1Choice1 = questions[i].choices[0];
+    var holdq1Choice2 = questions[i].choices[1];
+    var holdq1Choice3 = questions[i].choices[2];
+    var holdq1Choice4 = questions[i].choices[3];
+
+    orderListEl.innerHTML = '';
+
+    var liTag1 = document.createElement("li");
+    liTag1.setAttribute("class", "all_li")
+    var btn = document.createElement('button');
+    btn.setAttribute("class", "all_btn")
+    btn.textContent = holdq1Choice1;
+    liTag1.appendChild(btn)
+    orderListEl.appendChild(liTag1);
+    divContEL.appendChild(orderListEl);
+
+    var liTag2 = document.createElement("li");
+    liTag2.setAttribute("class", "all_li");
+    var btn2 = document.createElement('button');
+    btn2.setAttribute("class", "all_btn")
+    btn2.textContent = holdq1Choice2;
+    liTag2.appendChild(btn2)
+    orderListEl.appendChild(liTag2)
+    divContEL.appendChild(orderListEl);
+
+    var liTag3 = document.createElement("li");
+    liTag3.setAttribute("class", "all_li")
+    var btn3 = document.createElement('button');
+    btn3.setAttribute("class", "all_btn")
+    btn3.textContent = holdq1Choice3;
+    liTag3.appendChild(btn3)
+    orderListEl.appendChild(liTag3)
+    divContEL.appendChild(orderListEl);
+
+    var liTag4 = document.createElement("li");
+    liTag4.setAttribute("class", "all_li")
+    var btn4 = document.createElement('button');
+    btn4.setAttribute("class", "all_btn");
+    btn4.textContent = holdq1Choice4;
+    liTag4.appendChild(btn4);
+    orderListEl.appendChild(liTag4);
+    divContEL.appendChild(orderListEl);
+    var allBtnEl = document.querySelectorAll(".all_btn")
+    allBtnEl.forEach(function (event) {
+        event.addEventListener("click", onclickHandler)
+    });
+}
+
+// Set timer 
+var timer = 76;
+var timeCount;
+
+// Function for timer to count down as soon as the quiz starts 
+function setupTimer() {
+    timeCount = setInterval(function () {
+        timer--;
+        var timeReset = timeElement.textContent = "Time:" + " " + timer;
+       timer = timer;
+        if (timer <= 0) {         
+            clearInterval(timeCount);
+              
+            timeElement.textContent = timeReset;
+             
+        }
+    }, 1000)
+}
+
+// EventListener starts timer and hides the quiz button 
+document.addEventListener("click", function (event) {
+    if (event.target === btnElement) {
+        wrapperElement.style.display = "none";
+        setupTimer()
+        displayQuestions();
+    }
+})
+
+var i = 0; 
+function onclickHandler(event) {
+     
+    if(timer<=0){
+        clearInterval(timeCount);
+        divContEL.style.display="none";
+        displayResult();
+    }
+
+    var answerText = event.target.textContent 
+    if (answerText === questions[i].answer) {
+        timer = timer;
+        responsDiv.setAttribute("style", "color: green")
+        responsDiv.textContent = "Correct";
+    } else {
+        responsDiv.setAttribute("style", "color: red")
+        responsDiv.textContent = "Wrong";
+        timer = timer - 15;
+     }
+    
+     if (i < questions.length-1) {
+        i++;
+        setTimeout(function () {
+        displayQuestions();
+        responsDiv.textContent = "";
+    }, 1000)
+    }else {
+        setTimeout(function () {
+            responsDiv.textContent = "";
+            displayResult();
+            clearInterval(timeCount);
+        }, 500)
+        
+        divContEL.innerHTML = '';
+     }
+     
+// Function to display users final score 
+    function displayResult() {
+        finishDiv.style.visibility = "visible";
+        timeElement.textContent = "Time:" + " " + timer;
+        var HighScores = timer;
+        localStorage.getItem(HighScores)
+        finalScore.textContent = "Your finally score is: " + HighScores;
+         localStorage.setItem("HighScores", HighScores)
+        }
+    }
+// Function to show the last page  
+    function renderLastItem() {
+    var yourScore = localStorage.getItem("HighScores");
+     var yourInitial = localStorage.getItem("Initial");
+     if (yourScore && yourInitial === "") {
+        return
+    }
+    finishDiv.textContent = "";
+    var finaPageEl = document.querySelector(".final-page");
+    finaPageEl.style.visibility = "visible";
+    var initialAndScore = document.querySelector("#staticEmail");
+    initialAndScore.value = yourInitial + ":" + " " + yourScore;
+    }
+
+    document.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var initialInput = document.querySelector("#inputInitial").value;
+    if (initialInput === "") {
+        errMsg.setAttribute("style", "color: red")
+        errMsg.textContent = "Initial input field cannot be empty"
+    } else {
+        errMsg.textContent = "";
+        localStorage.getItem(initialInput)
+        localStorage.setItem("Initial", initialInput)
+         renderLastItem()
+        }
+    })
+
+// This function will refresh the page and send user back to begining page when go back button is clicked 
+function init() {
+     location.reload();
+}
+
+// This function will  clear initial and score displayed on the final page
+function clearScore() {
+    initialAndScore.value = "";
+}
---
+++ README.md
@@ -0,0 +1,47 @@
+# Code Quiz
+## Description
+This app is timed coding quiz with multiple-choice questions. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that you write.
+
+## Table of Contents
+
+- [Usage](#usage)
+- [Links](#Links)
+- [Credits](#credits)
+- [License](#license)
+- [Contact](#Contact)
+
+
+## Usage
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+![Alt text](image.png)
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+![Alt text](image-2.png)
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+![Alt text](image-3.png)
+WHEN the game is over
+THEN I can save my initials and score
+
+## Links
+
+
+## Credits
+Credit to my tutor. She has been helpful in helping me understand many of the complex topics. My instructor for teaching it to us and Google/Stackoverflow when I got errors I was able to troubleshoot.
+## License
+ [https://choosealicense.com/](https://choosealicense.com/).
+${renderLicenseLink (data.license)} 
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+## Badges
+${renderLicenseBadge (data.license)}
+
+## Contact
+mariayaslinskiy@gmail.com
+YaslinskiyM Github
